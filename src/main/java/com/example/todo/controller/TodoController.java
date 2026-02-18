1405dcb944a1df87d6217634a0289f2accc66323
TaMa <202510_aw_tani@x-ide.sakura.ne.jp>
2026-02-18 14:11:21 +0900

    一覧画面の作成

---
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -0,0 +1,24 @@
+package com.example.todo.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/todo")
+public class TodoController {
+
+    @GetMapping
+    public String list(Model model) {
+        List<String> todos = List.of(
+                "買い物に行く",
+                "Spring Bootの勉強をする",
+                "メールを返信する"
+        );
+        model.addAttribute("todos", todos);
+        return "todo/list";
+    }
+}
